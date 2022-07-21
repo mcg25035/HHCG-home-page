616ed41f740432f47f32cde7417698d051a970a6
南極雄 <70533278+mcg25035@users.noreply.github.com>
2022-07-22 03:54:52 +0800

    Add files via upload

---
+++ HHCG home page/index.php
@@ -0,0 +1,14 @@
+<head>
+<title>哈哈指令團首頁</title>
+<link rel="icon" href="https://HHCGhologramgenerator.mcg25035.repl.co/icon.ico" type="image/x-icon" />
+
+</head>
+<body>
+<p><img style="float: left;" src="https://drive.google.com/uc?export=view&amp;id=1wxRWupm6qH-QvlM-YSKnCzklu7JloIHg" alt="" /><img style="float: right;" src="https://drive.google.com/uc?export=view&amp;id=1ReAWKInllJjScTr0eh_Cfhqwb8imv4Uj" alt="" /><br /><br /><br /></p>
+<hr />
+<div>| <a title="物品指令產生器" href="https://cutt.ly/HHCG-ItemCreater" target="_blank" rel="noopener"><span style="text-decoration: underline;"><span style="color: #000000; text-decoration: underline;">物品指令產生器</span></span></a> | <span style="color: #000000;"><a style="color: #000000;" title="漂浮文字指令產生器" href="https://cutt.ly/HHCG-HoloGenerator" target="_blank" rel="noopener">漂浮文字指令產生器</a></span> | 驢子物品選單產生器(開發中敬請期待) | ...... |</div>
+<hr />
+<p><strong>本網站最新快訊</strong><br />哈哈指令團官方網站架設完成！<br />並且提供一些產生器供使用！</p>
+<p><strong>指令界快訊<br /></strong>/team指令存在一個很嚴重的漏洞。 -2021.02.22 1:41 (台北時間) <span style="text-decoration: underline;">|由Alex Cai投稿|</span><br /><small><code>好各位，我知道現在是凌晨一點，但是我突然覺得，我有義務要把這個訊息傳遞出去。</code><br /><code>隊伍系統中，存在著一個選項 collisionRule，決定了該隊伍的推擠情況；使用方法是/team modify &lt;隊伍名&gt; collisionRule &lt;狀態&gt;</code><br /><code>狀態預設是always，亦即這個隊伍永遠會推擠其他實體；也可以設成never，使隊伍完全不推擠其他實體。</code><br /><code>問題來了。</code><br /><code>假設今天想要使隊伍只推擠非隊伍的實體，隊伍內不互相推擠怎麼辦？</code><br /><code>對/team指令較熟的朋友可能會說，那就用/team modify &lt;隊伍名&gt; collisionRule pushOtherTeams啊？</code><br /><code>不行。因為這就是漏洞所在。</code><br /><code>pushOtherTeams 和 pushOwnTeam 的程式寫反了！</code><br /><code>沒錯，就是字面意義，pushOtherTeams不再是 「推擠 其他 隊伍」，pushOwnTeam才是；反之亦然。</code><br /><code>這個漏洞最早在2015年前的15w36c，1.9的先行版就被發現。</code><br /><code>時至今日，五年過去了，仍未被修復。</code><br /><code>我不懂Mojang在想什麼，是由於修復有太大的困難，還是他們以為每個人都知道這個漏洞了，乾脆將錯就錯？</code><br /><code>在下敝人我的地圖也曾被這個bug坑了，今天我一定要將這個訊息盡量告訴更多的人，讓大家免於這個困擾。</code><br /><code>附上連結：<a role="link" href="https://bugs.mojang.com/browse/MC-87984" target="_blank" rel="nofollow noopener">https://bugs.mojang.com/browse/MC-87984</a></code></small></p>
+<p><strong>指令投稿作品<br /></strong>看起來這裡什麼都沒有，歡迎到此網址投稿指令作品，填補這個空白處。</p>
+<p>&nbsp;</p></html>
